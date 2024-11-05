86236a9ba57eac8ae17047ab842d75c2f4356799
luridevs <luridevs@gmail.com>
2024-11-04 21:44:47 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import openai
+from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer, util
+import pinecone  # For vector storage and RAG pipeline
+import faiss
 
-st.title("🎈 My new app")
+st.title("Outliner")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Tired of looking through and re-reading your notes for hours? Just put in the new notes you've taken, or start from scratch anytime, and Outliner will come up with a comprehensive, thorough outline."
 )
+
+
